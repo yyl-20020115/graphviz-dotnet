085a102f1b3c733577c9669dc14337cc22e2eb43
john2015 <yyl_20050115@hotmail.com>
2018-04-26 23:21:25 +0800

    添加项目文件。

---
+++ WinDot/_MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinDot
+{
+    public partial class _MainForm : Form
+    {
+        public _MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string txt = System.IO.File.ReadAllText("Network.gv");
+            GraphResult g = GraphProvider.Generate(txt,"dot","jpg");
+            
+            if (g != null)
+            {
+                g.Dispose();
+            }
+
+        }
+    }
+}
